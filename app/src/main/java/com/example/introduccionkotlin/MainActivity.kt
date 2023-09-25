a3a826aaaaa0267b3b90966da27e566035c6f241
RobertoBoz <roberto.bolanos@it-strategy.mx>
2023-09-25 15:33:59 -0600

    Diferencia entre variables constantes y valores

--- app/src/main/java/com/example/introduccionkotlin/MainActivity.kt
+++ app/src/main/java/com/example/introduccionkotlin/MainActivity.kt
@@ -3,9 +3,31 @@ package com.example.introduccionkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
+
+    /* Las constantes se agrega al companion object se declara fuera del proceso de creación de la pantalla para poder acceder a ellas desde
+        cualquier punto de la aplicación */
+    companion object {
+        // Declarar constantes no son aplicables a variables locales
+        const val moneda = "EUR"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //  Los valores sé declararán con val son pueden tener cambios en tiempo de ejecución
+        val fecha = "99/99/1999"
+        // Las variables declaradas con var son las que pueden tener cambios en tiempo de ejecución
+        var nombre = "Roberto"
+        // Para imprimir como en todos los lenguajes se ocupa println o  print
+        println("$nombre $fecha")
+        // Cambiamos el nombre por otro e imprimimos de nuevo
+        nombre = "Alberto"
+        println("$nombre $fecha")
+        // Al intentar cambiar la fecha se generara un error
+        println(moneda)
+
     }
 }
