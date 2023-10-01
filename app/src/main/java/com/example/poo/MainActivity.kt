d90f5808841e4d537bf61d956b3e7ef38f04aa4f
RobertoBoz <roberto.bolanos@it-strategy.mx>
2023-10-01 16:48:22 -0600

    Constructores

--- app/src/main/java/com/example/poo/MainActivity.kt
+++ app/src/main/java/com/example/poo/MainActivity.kt
@@ -7,9 +7,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var person = Person()
-        println( person.alive)
-        person.die()
-        println( person.alive)
+        var rboz = Person("Roberto", null)
+
+        println( rboz.alive)
+        println( rboz.name)
+        println( rboz.passport)
+        rboz.die()
+        println( rboz.alive)
+
+        var anonimo = Person()
+        println( anonimo.alive)
+        println( anonimo.name)
+        println( anonimo.passport)
+        anonimo.die()
+        println( anonimo.alive)
+
+        anonimo.Person()
+        println( anonimo.alive)
+        println( anonimo.name)
+        println( anonimo.passport)
+        anonimo.die()
+        println( anonimo.alive)
     }
 }
--- app/src/main/java/com/example/poo/Person.kt
+++ app/src/main/java/com/example/poo/Person.kt
@@ -1,8 +1,13 @@
 package com.example.poo
 
-class Person {
+class Person(var name:String = "Anonimo", var passport:String? = null) {
     var alive: Boolean = true
     fun die(){
         alive = false
+    }
+    fun Person(){
+        name = "Juan"
+        passport = "1234"
+        alive = true
     }
 }
